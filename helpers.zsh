5d10a46ea3f22963ec3a63565af0abc97c79bc56
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2019-08-23 17:13:16 +0430

    .

--- helpers.zsh
+++ helpers.zsh
@@ -5,3 +5,11 @@ alias cy='cyph  | color 255 140 10'
 cyn() { interrogatrix.py show-node -e "$@" | cyph }
 cyr() { interrogatrix.py show-rel -e "$@" | cyph }
 cyrm-all() { cyph 'match (n) detach delete n;' }
+cyavatar() {
+    local res="$(cyph --format plain "match (user:User {username: TOLOWER('$1')})-[:HAS_AVATAR]->(photo:Photo)
+return photo.photourl")"
+    local url="${${${(@f)res}[2]}[2,-2]}"
+    re dvar res url
+    aa --dir ./$1/ "$url"
+    image ./$1/*
+}
